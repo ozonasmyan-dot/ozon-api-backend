34239bd7cf67f24483b19465fc1d820460e207cd
ozonasmyan-dot <ozon.asmyan@gmail.com>
2025-08-24 13:09:58 +0400

    feat: configure env vars

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const requireEnv = (name: string): string => {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${name}`);
+  }
+  return value;
+};
+
+export const PERFORMANCE_CLIENT_ID = requireEnv('OZON_PERFORMANCE_CLIENT_ID');
+export const PERFORMANCE_CLIENT_SECRET = requireEnv('OZON_PERFORMANCE_CLIENT_SECRET');
+export const SELLER_CLIENT_ID = requireEnv('OZON_SELLER_CLIENT_ID');
+export const SELLER_API_KEY = requireEnv('OZON_SELLER_API_KEY');
+export const CORS_ORIGIN = requireEnv('CORS_ORIGIN');
+export const PORT = parseInt(requireEnv('PORT'), 10);
+
